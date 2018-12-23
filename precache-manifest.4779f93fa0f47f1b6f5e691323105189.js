ac5d624b3b957ef94dc410d8388ff01fae545c58
Mohsen <mohsen.1044@gmail.com>
2018-12-23 12:35:29 +0330

    Updates

---
+++ precache-manifest.4779f93fa0f47f1b6f5e691323105189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11649defc77c6be0c18b",
+    "url": "/maktabkhooneh/static/css/main.8900e09d.chunk.css"
+  },
+  {
+    "revision": "11649defc77c6be0c18b",
+    "url": "/maktabkhooneh/static/js/main.11649def.chunk.js"
+  },
+  {
+    "revision": "6f610aabcdb291eb5d79",
+    "url": "/maktabkhooneh/static/js/1.6f610aab.chunk.js"
+  },
+  {
+    "revision": "d4e6769ffb4e929247c5",
+    "url": "/maktabkhooneh/static/js/runtime~main.d4e6769f.js"
+  },
+  {
+    "revision": "3bd7b1d4ac5ecc215a0382452b4bd2f3",
+    "url": "/maktabkhooneh/index.html"
+  }
+];
